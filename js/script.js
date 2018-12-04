9386fae70d332125cc1135ccc15fd55bdf206418
Mikman83 <ivlev.michael@gmail.com>
2018-12-04 13:07:57 +0300

    микроправки замечаний по задаче №2

--- js/script.js
+++ js/script.js
@@ -71,17 +71,17 @@ ready(function(){
   const cardsWrap = document.querySelector('.catalog__list');
 
   function createCards(object) {
-    const card = document.createElement('div');
+    const card = document.createElement('article');
     card.classList.add('product-card-mini');
 
     card.innerHTML =`<h2 class="product-card-mini__title">
-            <a href="${object.href}">${object.title}</a>
-          </h2>
-          <a href="${object.href}" class="product-card-mini__img-wrap">
-            <img src="${object.src}" alt="some picture" class="product-card-mini__img">
-          </a>
-          <p class="product-card-mini__descr">${object.descr}</p>
-          <div class="product-card-mini__price">${object.price}</div>`;
+        <a href="${object.href}">${object.title}</a>
+      </h2>
+      <a href="${object.href}" class="product-card-mini__img-wrap">
+        <img src="${object.src}" alt="some picture" class="product-card-mini__img">
+      </a>
+      <p class="product-card-mini__descr">${object.descr}</p>
+      <div class="product-card-mini__price">${object.price}</div>`;
 
     return card;
   }
